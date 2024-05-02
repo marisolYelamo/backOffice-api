1e74e18739c3e950c57388191bc16a3b091a5ded
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2024-05-02 18:05:15 -0300

    edit field of register and add login fuction

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,20 @@
 import AuthDao from "../daos/auth.dao";
-
+import { comparePassword } from "../utils/bcrypt";
+import { createJWT } from "../utils/jwt";
 class AuthService {
   static async register(body) {
     const newUser = await AuthDao.register(body);
     return newUser;
+  }
+  static async login(email, password) {
+    const user = await AuthDao.getUser(email);
+    const validPassword = await comparePassword(password, user.password);
+    if (!validPassword) {
+      throw Error("invalid password");
+    } else {
+      const token = createJWT(user, "", "365d");
+      return { token, email: user.email };
+    }
   }
 }
 
