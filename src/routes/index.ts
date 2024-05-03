2f1880deead6452f7281b1ba4aa1e7e58af57b21
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2024-05-03 14:41:55 -0300

    delete verify roles on Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,14 +6,12 @@ import UsersRoutes from "./users";
 import AuthRoutes from "./auth";
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware";
-import acl from "../acl";
 
 const router = express.Router();
 router.use("/auth", AuthRoutes);
 router.use("/users", authMiddleware, UsersRoutes);
 // Routes with access for logged in user
 
-router.use(acl.authorize);
 router.use("/data", authMiddleware, DataRoutes);
 router.use("/roles", authMiddleware, RoleRoutes);
 
