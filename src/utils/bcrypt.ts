22c5c452bdc8cf430e31f3ff7777aab908ae0d19
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2024-05-08 15:59:53 -0300

    update login

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,13 +1,20 @@
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const hashPassword = async (password) => {
   const salt = await bcrypt.genSalt();
   const hash = await bcrypt.hash(password, salt);
   return hash;
 };
-
-const comparePassword = async (password, hash) => {
-  return bcrypt.compare(password, hash);
+const rebuildPasswordHash = (password, salt) => {
+  return crypto
+    .createHmac("sha1", salt)
+    .update(salt + password)
+    .digest("hex");
+};
+const comparePassword = async (password, salt, hash) => {
+  const rebuildPass = rebuildPasswordHash(password, salt);
+  return rebuildPass === hash;
 };
 
-export { hashPassword, comparePassword };
+export { hashPassword, comparePassword, rebuildPasswordHash };
